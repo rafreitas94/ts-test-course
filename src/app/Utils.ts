b57951a3d9e31b33ed28b17f87e1052c102e460e
Raphael <raphaelsfreitas94@gmail.com>
2022-05-07 18:09:38 -0300

    class 11 - coverage

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -7,12 +7,13 @@ export class Utils {
     public static parseUrl(url: string): UrlWithParsedQuery {
         if (!url) {
             throw new Error('Empty url!');
-            
+
         }
         return parse(url, true);
     }
     
 
+    /* istanbul ignore next */
     public static toUpperCase(arg: string) {
         return arg.toUpperCase();
     }
