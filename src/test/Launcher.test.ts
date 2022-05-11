66304bb356c77ff8d98898150cd32073a45ff041
Raphael <raphaelsfreitas94@gmail.com>
2022-05-10 23:54:46 -0300

    class 25 - deep mocks

---
+++ src/test/Launcher.test.ts
@@ -0,0 +1,30 @@
+import { mocked } from 'ts-jest/utils';
+import { Launcher } from '../app/Launcher';
+import { Server } from '../app/Server/Server';
+
+jest.mock('../app/Server/Server', () =>{
+    return {
+        Server: jest.fn(()=>{
+            return {
+                startServer:() =>{
+                    console.log('starting fake server!');
+                }
+            }
+        })
+    }
+})
+
+describe('Launcher test suite', () => {
+    const mockedServer = mocked(Server, true);
+    test('create server', () => {
+        new Launcher();
+        expect(mockedServer).toBeCalled();
+    });
+
+    test('launch app', () => {
+        const launcherAppMock = jest.fn();
+        Launcher.prototype.launchApp = launcherAppMock;
+        new Launcher().launchApp();
+        expect(launcherAppMock).toBeCalled();
+    });
+});
